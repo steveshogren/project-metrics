15e826fc7901fad4b171adb42eba21cf8fd709ea
Jack <steve.a.shogren@gmail.com>
2016-01-01 08:54:38 -0500

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,22 @@
+module Lib
+    ( someFunc
+    ) where
+
+import System.Environment
+import System.Exit
+
+someFunc :: IO ()
+someFunc = getArgs >>= parse
+
+parse :: [String] -> IO ()
+parse ["-h"] = usage  >> exit
+parse [] = usage >> exit
+
+usage :: IO ()
+usage   = putStrLn "Usage: dateParser \n [-v version]\n [-h help]\n [-c find oldest missing]\n [-b print bash gui] \n [-w write bash file] \n [-u update all git hooks] \n [-r find rebase commits in current]"
+
+exit :: IO a
+exit    = exitSuccess
+
+die :: IO a
+die     = exitWith (ExitFailure 1)
