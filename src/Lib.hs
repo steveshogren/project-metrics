bfd8b7b5f0a871226b2d4f0dad849dd8c456c5f9
Jack <steve.a.shogren@gmail.com>
2016-01-01 15:14:08 -0500

    Adding the report generation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Lib
     ( someFunc
     ) where
@@ -5,10 +10,28 @@ module Lib
 import System.Environment
 import System.Exit
 import System.Process (readProcess)
+import Text.Hamlet
+import Text.Blaze.Html.Renderer.String (renderHtml)
+import Text.Blaze.Html
+
+renderTemplate :: String -> String -> String -> String
+renderTemplate testVariable exit other = renderHtml ( $(shamletFile "mypage.hamlet") )
+
+s = writeFile "./report.html" $ renderTemplate "foobar" "[1, 2], [3, 4]"  "[1, 5], [2, 4]"
+
+m :: IO ()
+m = putStrLn $ renderHtml [shamlet|
+<p>Hello, my name is #{show 3} and I am #{show 4}.
+<p>
+    Let's do some funny stuff with my name: #
+    <b>#{show 4}
+<p>Oh, and in 5 years I'll be #{show 3} years old.
+|]
 
 grepRepo ::  IO String
 grepRepo = do
-  readProcess "git" ["grep", "exit"] "."
+  output <- readProcess "git" ["grep", "exit"] "."
+  (return . show . length . lines) $ output
 
 someFunc :: IO ()
 someFunc = getArgs >>= parse
@@ -16,6 +39,8 @@ someFunc = getArgs >>= parse
 parse :: [String] -> IO ()
 parse ["-h"] = usage  >> exit
 parse ["-a"] = (grepRepo >>= putStrLn) >> exit
+parse ["-s"] = s >> exit
+parse ["-m"] = m >> exit
 parse [] = usage >> exit
 
 usage :: IO ()
