de4009d0e096a51787e615d6a69057de9067ee77
Jack <steve.a.shogren@gmail.com>
2016-01-03 17:04:53 -0500

    Adding the ability to update from grep

--- src/Lib.hs
+++ src/Lib.hs
@@ -36,10 +36,8 @@ load f = do s <- readFile f
 save :: (Show a) => a -> FilePath -> IO ()
 save x f = writeFile f (show x)
 
-roundTrip :: IO MetricHistory
-roundTrip =
-  save [(CountDate{date="today",count=1})] "db.txt"
-     >> load "db.txt"
+clearFile :: String -> IO ()
+clearFile f = save ([]::MetricHistory) f
 
 addMetric :: String -> Int -> IO MetricHistory
 addMetric file count = do
@@ -51,31 +49,21 @@ addMetric file count = do
 renderTemplate :: String -> String -> String -> String
 renderTemplate testVariable exit other = renderHtml ( $(shamletFile "mypage.hamlet") )
 
-s = writeFile "./report.html" $ renderTemplate "foobar" "[1, 2], [3, 4]"  "[1, 5], [2, 4]"
+generateHtml = writeFile "./report.html" $ renderTemplate "foobar" "[1, 2], [3, 4]"  "[1, 5], [2, 4]"
 
-m :: IO ()
-m = putStrLn $ renderHtml [shamlet|
-<p>Hello, my name is #{show 3} and I am #{show 4}.
-<p>
-    Let's do some funny stuff with my name: #
-    <b>#{show 4}
-<p>Oh, and in 5 years I'll be #{show 3} years old.
-|]
-
-grepRepo ::  IO String
+grepRepo :: IO Int
 grepRepo = do
   output <- readProcess "git" ["grep", "exit"] "."
-  (return . show . length . lines) $ output
+  (return . length . lines) $ output
 
 someFunc :: IO ()
 someFunc = getArgs >>= parse
 
 parse :: [String] -> IO ()
-parse ["-h"] = usage  >> exit
-parse ["-a"] = (grepRepo >>= putStrLn) >> exit
-parse ["-s"] = s >> exit
-parse ["-m"] = m >> exit
-parse ["-r"] = roundTrip >>= (putStrLn . show) >> exit
+parse ["-h"] = usage >> exit
+parse ["-a"] = (grepRepo >>= (addMetric "db.txt")) >> exit
+parse ["-s"] = generateHtml >> exit
+parse ["-c"] = clearFile "db.txt" >> exit
 parse [] = usage >> exit
 
 usage :: IO ()
